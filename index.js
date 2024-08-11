c02818035adc14d4051b44e67b88a3f68105990b
Nutti Saelor <nutti.sae@truedigital.com>
2024-08-11 14:52:33 +0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const cors = require("cors");
+const express = require("express");
+const mysql = require("mysql2/promise");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:8888"],
+  })
+);
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+const port = 8000;
+const secret = "mysecret";
+
+let conn = null;
+
+const initMySQL = async () => {
+  conn = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "tutorial",
+  });
+};
+
+app.listen(post, async () => {
+  await initMySQL();
+  console.log("Server started at port 8000");
+});
